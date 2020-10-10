19e4e2b6dda445490df9b630729aefbfea10efef
Anna Winkler <3526523+annawinkler@users.noreply.github.com>
2020-10-10 08:49:46 -0600

    Add default route. Add message when starting server.

--- main.go
+++ main.go
@@ -6,7 +6,20 @@ import (
 	"os"
 )
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Route not found\n"))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Running API v1\n"))
+}
+
 func main() {
+	fmt.Println("Starting server on localhost:11111")
+	http.HandleFunc("/", rootHandler)
 	err := http.ListenAndServe("localhost:11111", nil)
 	if err != nil {
 		fmt.Println(err)
